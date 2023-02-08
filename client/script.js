7db42c680a2f116d352fe1520ee9fb9dc35c07bf
netadpros <jared@netadpros.com>
2023-02-08 11:47:08 -0700

    commit

--- client/script.js
+++ client/script.js
@@ -79,7 +79,8 @@ loader(messageDiv);
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-cr69.onrender.com', 
+  {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
